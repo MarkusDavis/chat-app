3208ddece0a46cff6eb3c853b81152f5278eae43
Markus Davis <85118308+MarkusDavis@users.noreply.github.com>
2022-09-10 13:11:50 -0400

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVyAsaRoY5EOjNcDS6GN5bnqBhzPudec0",
-  authDomain: "markus-davis-business.firebaseapp.com",
-  databaseURL: "https://markus-davis-business-default-rtdb.firebaseio.com",
-  projectId: "markus-davis-business",
-  storageBucket: "markus-davis-business.appspot.com",
-  messagingSenderId: "995422433233",
-  appId: "1:995422433233:web:cfd2f276f3dea0da4dfaf0",
-  measurementId: "G-E6FS4B4JXF"
+// API KEYS
 };
 
 // Initialize Firebase
